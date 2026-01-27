aa7c64a999d6ebde24704146d82113f98247de07
TekfolioNG <tekfoilong@gmail.com>
2026-01-27 03:13:36 +0100

    try afaunn

---
+++ .history/app_20260127030219.vue
@@ -0,0 +1,11 @@
+<template>
+  <UApp>
+    <ClientOnly>
+      <AppLoading v-if="showPreloader" @loaded="handlePreloaderLoaded" />
+    </ClientOnly>
+    <NuxtLoadingIndicator />
+    <NuxtLayout v-show="!showPreloader">
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
